66ff9f0766134636bd16b46b937d2e659e378b29
Karshyga07 <karshygaburkitbay3@gmail.com>
2026-05-03 20:48:07 +0500

    Karshyga

--- SourceCode/ResearchPaper.java
+++ SourceCode/ResearchPaper.java
@@ -1,22 +1,77 @@
-
 import java.io.*;
 import java.util.*;
-import java.time.LocalDate;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
 
 public class ResearchPaper implements IPublishable {
+    private String name;
+    private List<String> authors;
+    private int pages;
+    private int citations;
+    private LocalDate date;
+    private String DOI;
 
-    /*
-    Default constructor
-    */
-    public ResearchPaper() {
+    public ResearchPaper(String name, List<String> authors, int pages, int citations, LocalDate date, String DOI) {
+        this.name = name;
+        this.authors = authors;
+        this.pages = pages;
+        this.citations = citations;
+        this.date = date;
+        this.DOI = DOI;
     }
 
+    public String getName() {
+        return name;
+    }
 
-    public String name;
-    public List<String> authors;
-    public int pages;
-    public int citations;
-    public LocalDate date;
-    public String DOI;
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getCitations() {
+        return citations;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getDOI() {
+        return DOI;
+    }
+
+   
+    public void publish() {
+        System.out.println("Research paper published: " + name);
+    }
+
+    
+    public String toString() {
+        return "ResearchPaper{" +
+                "name='" + name + '\'' +
+                ", authors=" + authors +
+                ", pages=" + pages +
+                ", citations=" + citations +
+                ", date=" + date +
+                ", DOI='" + DOI + '\'' +
+                '}';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ResearchPaper)) return false;
+        ResearchPaper that = (ResearchPaper) o;
+        return Objects.equals(DOI, that.DOI);
+    }
+
+    
+    public int hashCode() {
+        return Objects.hash(DOI);
+    }
 }
--- SourceCode/Researcher.java
+++ SourceCode/Researcher.java
@@ -1,29 +1,81 @@
-
 import java.io.*;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Researcher {
+    private int hIndex;
+    private String school;
 
-    /*
-    Default constructor
-    */
-    public Researcher() {
+    private Researcher supervisor;
+    private List<ResearchProject> researchProjects;
+    private List<ResearchPaper> researchPapers;
+
+    public Researcher(int hIndex, String school) {
+        this.hIndex = hIndex;
+        this.school = school;
+        this.researchProjects = new ArrayList<>();
+        this.researchPapers = new ArrayList<>();
     }
 
-
-    public int hIndex;
-    public String school;
-    public Set<ResearchProject> researchProject;
-    public Set<ResearchPaper> researchPaper;
-
-
-    public void PrintResearches(Comparator c) {
-        // TODO implement here
+    public int getHIndex() {
+        return hIndex;
     }
 
+    public String getSchool() {
+        return school;
+    }
+
+    public Researcher getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Researcher supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public List<ResearchProject> getResearchProjects() {
+        return researchProjects;
+    }
+
+    public List<ResearchPaper> getResearchPapers() {
+        return researchPapers;
+    }
+
+    public void addResearchProject(ResearchProject project) {
+        researchProjects.add(project);
+    }
+
+    public void addResearchPaper(ResearchPaper paper) {
+        researchPapers.add(paper);
+    }
+
+    public void printPapers(Comparator<ResearchPaper> comparator) {
+        List<ResearchPaper> sortedPapers = new ArrayList<>(researchPapers);
+        sortedPapers.sort(comparator);
+
+        for (ResearchPaper paper : sortedPapers) {
+            System.out.println(paper);
+        }
+    }
 
     public int calculateTotalCitations() {
-        // TODO implement here
-        return 0;
+        int total = 0;
+
+        for (ResearchPaper paper : researchPapers) {
+            total += paper.getCitations();
+        }
+
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Researcher{" +
+                "hIndex=" + hIndex +
+                ", school='" + school + '\'' +
+                ", totalCitations=" + calculateTotalCitations() +
+                '}';
     }
 }
